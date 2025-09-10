e960f6ffe01a359c4e2149bbf52ae361fc168601
TGLBeut <Sgre9381@gmail.com>
2025-09-10 08:31:59 -0400

    adjusted the code

--- Chapter2 in class assignment.cpp
+++ Chapter2 in class assignment.cpp
@@ -1,9 +1,27 @@
-
+/*File Name: Chapter2 in class assignment.cpp
+*Programmer: <Sean Greene>
+*Date: <9/10/2025>
+* Requirements:
+* Create a program that computes total sales tax on a $95 purchase.
+*set sales tax to 4 percent and county tax to 2 percent.
+*/
 
 #include <iostream>
+using namespace std;
+const double stateTax = .04;
+const double countyTax = .02;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+	double itemCost = 95.00;
+	double totaltax = itemCost * stateTax + itemCost * countyTax;
+	double totalCost = itemCost + totaltax;
 
+	cout << "The cost of the item is $" << itemCost << "\n";
+	cout << "The total tax including a state tax of " << stateTax << endl;
+	cout << " and a county tax of " << countyTax << endl;
+	cout << " You owe the store a total of " << totalCost;
+
+
+		return 0;
+}
